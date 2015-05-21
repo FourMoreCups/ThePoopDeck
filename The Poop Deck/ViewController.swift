7eb15201ac1137e080cd90e4b70bdea493d40f4e
Sean Deaton <sean@Seans-MacBook-Pro.local>
2015-05-21 17:28:35 -0400

    Initial Commit

---
+++ The Poop Deck/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  The Poop Deck
+//
+//  Created by Sean Deaton on 5/21/15.
+//  Copyright (c) 2015 Sean Deaton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
