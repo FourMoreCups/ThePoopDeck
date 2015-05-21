d4a3701b6ccdd8821374085fe635ded83dfc0070
Sean Deaton <sean@Seans-MacBook-Pro.local>
2015-05-21 17:44:15 -0400

    Initial Setup. Working iPhone App and WatchKit App. Completely new profiles, App Groups, etc.

---
+++ The Poop Deck WatchKit Extension/GlanceController.swift
@@ -0,0 +1,31 @@
+//
+//  GlanceController.swift
+//  The Poop Deck WatchKit Extension
+//
+//  Created by Sean Deaton on 5/21/15.
+//  Copyright (c) 2015 Sean Deaton. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class GlanceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ The Poop Deck WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  The Poop Deck WatchKit Extension
+//
+//  Created by Sean Deaton on 5/21/15.
+//  Copyright (c) 2015 Sean Deaton. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
