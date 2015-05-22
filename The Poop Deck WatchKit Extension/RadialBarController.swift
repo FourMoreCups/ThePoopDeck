3751a8c13747c614dbf3298283417033e5cfff84
Sean Deaton <sean@Seans-MacBook-Pro.local>
2015-05-21 22:03:10 -0400

    Working WatchKit App.

--- The Poop Deck WatchKit Extension/RadialBarController.swift
+++ The Poop Deck WatchKit Extension/RadialBarController.swift
@@ -11,4 +11,23 @@ import Foundation
 
 class RadialBarController: WKInterfaceController {
 
+    @IBOutlet weak var radialBarImage: WKInterfaceImage!
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+        self.radialBarImage.setImageNamed("DayCountDown")
+        self.radialBarImage.startAnimatingWithImagesInRange(NSMakeRange(0, 100), duration: 86400 * 14.25, repeatCount: 0)
+        
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
 }
