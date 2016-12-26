d4a853d14ac9b23fcf70c2bf560893ac656bcc32
Sean Deaton <sean@Seans-MacBook-Pro.local>
2016-12-26 10:37:25 -0500

    Moving to Swift 3

---
+++ The Poop Deck WatchKit App/WatchMenuRow.swift
@@ -0,0 +1,14 @@
+//
+//  WatchMenuRow.swift
+//  The Poop Deck
+//
+//  Created by Sean Deaton on 12/25/15.
+//  Copyright © 2015 Sean Deaton. All rights reserved.
+//
+
+import WatchKit
+
+class WatchMenuRow: NSObject {
+    @IBOutlet var menuLabel: WKInterfaceLabel!
+    
+}
