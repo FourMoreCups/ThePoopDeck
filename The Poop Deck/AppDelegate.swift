760f5da4e14aee0968f27198f8b3ff0d2245cabf
Sean Deaton <sean@seans-macbook-pro.local>
2016-12-30 12:43:53 -0500

    • Removed App Transport Security exception because I'm using an HTTPS connection. • Removed debug statements. • Submitting to app store.

--- The Poop Deck/AppDelegate.swift
+++ The Poop Deck/AppDelegate.swift
@@ -12,27 +12,8 @@ import WatchConnectivity
 @UIApplicationMain
 
 class AppDelegate: UIResponder, UIApplicationDelegate, WCSessionDelegate{
-    /** Called when all delegate callbacks for the previously selected watch has occurred. The session can be re-activated for the now selected watch using activateSession. */
-    @available(iOS 9.3, *)
-    public func sessionDidDeactivate(_ session: WCSession) {
-        //something here
-    }
-
-    /** Called when the session can no longer be used to modify or add any new transfers and, all interactive messages will be cancelled, but delegate callbacks for background transfers can still occur. This will happen when the selected watch is being changed. */
-    @available(iOS 9.3, *)
-    public func sessionDidBecomeInactive(_ session: WCSession) {
-        //something here
-    }
-
-    /** Called when the session has completed activation. If session state is WCSessionActivationStateNotActivated there will be an error with more details. */
-    @available(iOS 9.3, *)
-    public func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
-        //something here
-    }
-
 
     var window: UIWindow?
-    
 
     enum ShortcutType: String {
         case MealShortcut = "com.seandeaton.The-Poop-Deck.mealshortcut"
@@ -197,6 +178,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate, WCSessionDelegate{
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+    
+    /** Called when all delegate callbacks for the previously selected watch has occurred. The session can be re-activated for the now selected watch using activateSession. */
+    @available(iOS 9.3, *)
+    public func sessionDidDeactivate(_ session: WCSession) {
+        //something here
+    }
+    
+    /** Called when the session can no longer be used to modify or add any new transfers and, all interactive messages will be cancelled, but delegate callbacks for background transfers can still occur. This will happen when the selected watch is being changed. */
+    @available(iOS 9.3, *)
+    public func sessionDidBecomeInactive(_ session: WCSession) {
+        //something here
+    }
+    
+    /** Called when the session has completed activation. If session state is WCSessionActivationStateNotActivated there will be an error with more details. */
+    @available(iOS 9.3, *)
+    public func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        //something here
+    }
+
 }
 
 
